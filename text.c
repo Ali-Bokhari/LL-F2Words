0f8b41293cc8fe01fd1b4a7194b480e7d80056c8
Ali Bokhari <abokhari@uoguelph.ca>
2019-10-19 00:59:31 -0400

    txt2words works on my files

--- text.c
+++ text.c
@@ -52,12 +52,16 @@ struct node_struct *txt2words( FILE *fp ) {
   char *token;
 
   while ( fgets (str, 256, fp) ) {
-    /*token = strtok (str, " ");
-    while (token) {*/
-    while (strlen(str) > 0){
+    /*printf("%s\n", str);*/
+    if (*str == '\n') {
+      token = malloc (2);
+      strcpy (token, "\n\0");
+      listAppend(&final, token);
+      continue;
+    }
+    while (*str){
       token = get_word (&str);
       if (*str) {
-        printf("token: %s\n", token);
         listAppend(&final, token);
       }
     }
@@ -70,25 +74,29 @@ char *get_word (char **str) {
   char *final;
   char type;
   int i;
-  if (f == ' ' || f == '\n'){
+  if (f == ' '){
     *str = *str + 1;
     return get_word(str);
   }
-  if ((f > 96 && f < 123) || (f > 64 && f < 91) || (f > 47 && f < 58)) {
+  if ((f >= 'a' && f <= 'z') || (f >= 'A' && f <= 'Z') || (f >= '0' && f <= '9')) {
     type = 1;
   } else if (f == '\n') {
     type = 3;
-  } else if (f == *str[1]) {
+  } else if (f == (*str)[1]) {
     type = 2;
   } else {
-    type = 1;
+    type = 2;
   }
+
+  /*printf("f: %c type: %d\n", f, type);*/
 
   if (type == 1) {
     for (i = 1; i < 256; i++){
       f = str[0][i];
-      if (!(f > 96 && f < 123) && !(f > 64 && f < 91) && !(f > 47 && f < 58)) {
-        break;
+      if (!(f >= 'a' && f <= 'z') && !(f >= 'A' && f <= 'Z') && !(f >= '0' && f <= '9')) {
+        if (!(f == '-' && str[0][i+1] != '-') && !(f == '\'' && str[0][i+1] != '\'')) {
+          break;
+        }
       }
     }
     final = malloc (i + 1);
@@ -98,25 +106,26 @@ char *get_word (char **str) {
     printf("tester %s\n", *str);*/
     return final;
   } else if (type == 2) {
-    for (i = 1; i < 256; i++) {
-      if (f != *str[i]){
-        break;
+      for (i = 1; i < 256; i++) {
+        if (f != (*str)[i]){
+          break;
+        }
+        f = (*str)[i];
       }
-      f = *str[i];
-    }
-    final = malloc (i + 1);
-    strncpy (final, *str, i);
-    *str = &(str[0][i]);
-    return final;
+      final = malloc (i + 1);
+      strncpy (final, *str, i);
+      *str = &(str[0][i]);
+      return final;
   } else if (type == 3){
-    if (f == str[0][1]){
       final = malloc (2);
-      strcpy (final, "\n\0");
+      strncpy (final, *str, 1);
       *str = *str + 1;
       return final;
-    }
   } else {
-
+      final = malloc (2);
+      strncpy (final, *str, 1);
+      *str = *str + 1;
+      return final;
   }
   *str = *str + 1;
   return NULL;
@@ -154,7 +163,11 @@ struct node_struct *copy( struct node_struct *start, struct node_struct *end ) {
 
 int main(){
   struct node_struct *new = NULL;
-  struct node_struct *copied = NULL;
+  FILE *fp = fopen ("text.txt", "r");
+  new = txt2words(fp);
+  printList(new);
+  fclose(fp);
+  /*struct node_struct *copied = NULL;
   char *char1 = malloc(sizeof(char)*6);
   char *char2 = malloc(sizeof(char)*6);
   char *char3 = malloc(sizeof(char)*6);
@@ -170,6 +183,6 @@ int main(){
   copied = search (new, "char2", fake_strcmp);
   printList(new);
   printf("searched:\n");
-  searchPrintList(copied);
+  searchPrintList(copied);*/
   return 0;
 }
